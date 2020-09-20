2350ae65ee0b1397f4d0ae28fef6c845cf08c2ea
Tyler-Dunham <53841822+Tyler-Dunham@users.noreply.github.com>
2020-09-19 21:24:04 -0400

    Update funCommands.cs

--- Commands/funCommands.cs
+++ Commands/funCommands.cs
@@ -13,52 +13,6 @@ namespace Discord_Bot_Tutorial.Commands
 {
     class funCommands : BaseCommandModule
     {
-        [Command("dick")]
-        public async Task Dick(CommandContext ctx)
-        {
-            var author = ctx.Message.Author.Id;
-            ulong panda = 238471482009714688;
-
-            if (author == panda)
-            {
-                await ctx.Channel.SendMessageAsync($"No.");
-                return;
-            }
-
-            var mention = ctx.User.Mention;
-
-            Random rnd = new Random();
-
-            double dickSize = rnd.Next(0, 12) + Math.Round(rnd.NextDouble(), 2);
-
-            var flushedEmoji = DiscordEmoji.FromName(ctx.Client, ":flushed:");
-
-            await ctx.Channel.SendMessageAsync($"{mention} has a {dickSize} inch schlong {flushedEmoji}");
-        }
-
-        [Command("gay")]
-        public async Task Gay(CommandContext ctx)
-        {
-            var flushedEmoji = DiscordEmoji.FromName(ctx.Client, ":flushed:");
-            var mention = ctx.User.Mention;
-            ulong panda = 238471482009714688;
-            var author = ctx.Message.Author.Id;
-
-            if (author == panda)
-            {
-                await ctx.Channel.SendMessageAsync($"{mention} is bi {flushedEmoji}.");
-                return;
-            }
-
-            string[] sexualities = { "straight", "gay", "bi", "trans", "pansexual", "very ugly", "10/10 hottie", "asexual" };
-
-            Random rnd = new Random();
-
-            int randomChoice = rnd.Next(sexualities.Length + 1);
-
-            await ctx.Channel.SendMessageAsync($"{mention} is {sexualities[randomChoice]} {flushedEmoji}.");
-        }
-
         [Command("time")]
         [Description("Shows current time.")]
         public async Task ShowCurrentTime(CommandContext ctx)
